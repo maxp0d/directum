faa1a56ad61c14f0a79da6845e624e462f8212dc
Max Pod <podsizertsev@ya.ru>
2023-10-18 23:05:03 +0400

    Blank main menu

--- EventManager/Program.cs
+++ EventManager/Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace EventManager
+{
+    internal class Program
+    {
+        static void ShowMainMenu()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("| Менеджер мероприятий  |");
+            Console.WriteLine("|     Главное меню      |");
+            Console.WriteLine("------------------------- \n\n");
+            Console.WriteLine("[1] Добавить новую встречу");
+            Console.WriteLine("[2] Изменить данные о встрече");
+            Console.WriteLine("[3] Удалить существующую встречу");
+            Console.WriteLine("[4] Вывести расписание встреч");
+            Console.WriteLine("[5] Выход \n");
+        }
+
+        static int GetCommand()
+        {
+            Console.WriteLine("\nВведите номер команды и нажмите Enter \n");
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int command))
+            {
+                command = -1;
+            }
+            return command;
+        }
+
+        static void ShowErrorMessage(string errorMessage)
+        {
+
+            Console.WriteLine(errorMessage);
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int currentCommand = 0;
+            do
+            {
+                switch (currentCommand)
+                {
+                    case 0:
+                        ShowMainMenu();
+                        currentCommand = GetCommand();
+                        break;
+
+                    default:
+                        ShowErrorMessage("Ошибка при вводе команды. Для продолжения нажмите любую клавишу...");
+                        currentCommand = 0;
+                        break;
+                }
+            } while (currentCommand != 5);
+        }
+    }
+}
